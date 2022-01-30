5cafd60a2c233080174586d7599a5246d4ace9b0
Alex <oleksandrilnytskyi@gmail.com>
2022-01-30 14:21:58 +0200

    somesome

--- js/main.js
+++ js/main.js
@@ -520,6 +520,11 @@ const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
     anchor.addEventListener('click', (e) => {
+        mobileMenu.style = `
+        transition-duration: 300ms;
+        transform: translate(-100%,0);
+        `
+        body.style.overflow = 'visible'
         e.preventDefault()
         const blockId = anchor.getAttribute('href')
         document.querySelector('' + blockId).scrollIntoView({
